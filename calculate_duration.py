ab339e3c399f4abafb25f174e22af00a7ce2b7bb
Scott Parker <sparker@redhat.com>
2021-02-19 16:09:16 -0700

    initial commit

---
+++ calculate_duration.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# Copyright (c) 2021 [Scott Parker]
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+                    
+DOCUMENTATION = r'''
+---
+module: calculate_duration
+
+short_description: This module to add time on end of the passed in date and time.
+
+version_added: "1.0.0"
+
+description: This module to add time on end of the passed in date and time.
+
+options:
+    calculate_duration:
+      date: This is date and time to start appending duration (fmt: %Y-%m-%d %H:%M:%S )
+      duration: Number of units to add. (int)
+      jY-%m-%d
+    sample: '2021-02-19'
+
+author:
+    - Scott Parker (@gearboxscott)
+'''
+
+EXAMPLE = r'''
+# Calculate the End of the Patching Window
+- name: Calculate the End of the Patching Window
+  calculate_duration:
+    date: '2021-02-19 07:00'
+    duration: 20
+    units: hours
+  register: patch_window_close
+
+Produces:
+
+{
+    "changed": true,
+    "date_out": "2021-02-20",
+    "time_out": "03:00",
+    "full_out": "2021-02-20 03:00",
+    "invocation": {
+        "module_args": {
+            "date": "2021-02-19 07:00:00",
+            "duration": 20,
+            "units": "hours"
+        }
+    },
+    "_ansible_no_log": false
+}
+
+'''
+
+RETURN = r'''
+date_out:
+    description: Incremented Date
+    type: string
+    returned: %Y-%m-%d
+    sample: '2021-02-19'
+time_out:
+    description: Incremented Time associated with date_out date
+    type: string
+    returned: %H:%M
+    sample: '13:00'
+full_out:
+    description: Incremented Date and Time together
+    type: string
+    returned: %Y-%m-%d %H:%M
+    sample: '2021-02-19 13:00'
+'''
+
+from ansible.module_utils.basic import *
+from datetime import date, datetime, timedelta, time
+from sys import argv
+
+def main():
+
+    fields = {
+        "date": { "required": True, "type": "str" },
+        "duration": { "required": True, "type": "int" },
+        "units": {
+            "type": "str",
+            "choices": [ 'days', 'seconds', 'microseconds', 'milliseconds', 'minutes', 'hours', 'weeks' ],
+            "default": "hours"
+        },
+    }
+
+    module = AnsibleModule( argument_spec=fields )
+
+    format_date_in = "%Y-%m-%d %H:%M:%S"
+    formatted_date = datetime.strptime( module.params[ 'date' ], format_date_in )
+
+    if module.params[ 'units' ] == 'days':
+        end_time = formatted_date + timedelta( days=module.params[ 'duration' ] )
+    elif module.params[ 'units' ] == 'seconds':
+        end_time = formatted_date + timedelta( seconds=module.params[ 'duration' ] )
+    elif module.params[ 'units' ] == 'microseconds':
+        end_time = formatted_date + timedelta( microseconds=module.params[ 'duration' ] )
+    elif module.params[ 'units' ] == 'milliseconds':
+        end_time = formatted_date + timedelta( milliseconds=module.params[ 'duration' ] )
+    elif module.params[ 'units' ] == 'minutes':
+        end_time = formatted_date + timedelta( minutes=module.params[ 'duration' ] )
+    elif module.params[ 'units' ] == 'hours':
+        end_time = formatted_date + timedelta( hours=module.params[ 'duration' ] )
+    elif module.params[ 'units' ] == 'weeks':
+        end_time = formatted_date + timedelta( weeks=module.params[ 'duration' ] )
+    else:
+        module.fail_exit( msg="Units of " + module.params[ 'units' ] + " is Invalid!" ) 
+
+    format_full_out = "%Y-%m-%d %H:%M" 
+    format_date_out = "%Y-%m-%d"
+    format_time_out = "%H:%M"
+
+    date_out = end_time.strftime( format_date_out )
+    time_out = end_time.strftime( format_time_out )
+    full_out = end_time.strftime( format_full_out )
+
+    module.exit_json( changed=True, date_out=date_out, time_out=time_out, full_out=full_out ) 
+
+if __name__ == '__main__':
+    main()
